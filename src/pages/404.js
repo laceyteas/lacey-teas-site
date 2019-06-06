45138f6a74ae7029db2fd68dae72e95c39182af9
Lacey Teas <laceyteas@protonmail.com>
2019-06-05 19:42:12 -0700

    Revised 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import Layout from '../common/layouts/Layout/Layout';
 //import Img from 'gatsby-image';
 import { graphql, Link } from 'gatsby';
 import Seo from '../common/seo';
+import Page from '../common/layouts/Page/Page'
 
 
 export default ({props, data}) => (
@@ -10,19 +11,12 @@ export default ({props, data}) => (
     <Seo
       title={`About ${data.site.siteMetadata.title}`}
       description={data.site.siteMetadata.description} />
- 
-    <div className="mw9 center flex flex-wrap pv5-l w-100">
-      <div className="mw7 w-100 pa2">
 
-        <h1 className="display fw1 db lh-copy"> 404 // Page Not Found</h1>
-
-        <p className="mw7 w-100 lh-copy serif pa2 flex flex-column justify-center f4" >        We couldn&#39;t find what you were looking for.</p>
-        <Link to="/" className="dib bg-dark-gray b near-white hover-bg-mid-gray pv3 ph4 ttu tracked sans-serif no-underline mv2">Go to homepage</Link>
-      </div>
-
-    </div>
+    <Page title="404">
+      <p>We couldn&#39;t find what you were looking for.</p>
+      <Link to="/" >Go to homepage</Link>
+    </Page>
   </Layout>
-
 
 )
 
