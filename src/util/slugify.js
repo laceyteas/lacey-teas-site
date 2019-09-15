56a3255a3d23f94183448f0272c2782d94980d2f
Lacey Teas <laceyteas@protonmail.com>
2019-09-15 12:25:22 -0700

    revised slugify

--- src/util/slugify.js
+++ src/util/slugify.js
@@ -1,11 +1,12 @@
-const slugify = (string) => {
+const slugify = (string,options = {}) => {
+    const {space = ''} = options; // What to replace a space with. Default is nothing. Dashs and Underlines are valid options.
     const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;'
     const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------'
     const p = new RegExp(a.split('').join('|'), 'g')
     return string.toString().toLowerCase()
-      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(/\s+/g, space) // Replace spaces with -
       .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-      .replace(/&/g, '-and-') // Replace & with ‘and’
+      .replace(/&/g, space + 'and' + space) // Replace & with ‘and’
       .replace(/[^\w-]+/g, '') // Remove all non-word characters
       .replace(/--+/g, '-') // Replace multiple - with single -
       .replace(/^-+/, '') // Trim - from start of text
