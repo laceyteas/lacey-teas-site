c7e203723226b1fbd5f6caf09eba0c7f4c79956d
Lacey Teas <laceyteas@protonmail.com>
2019-09-15 14:49:38 -0700

    removed netlify honeypot

--- src/common/components/Form/Form.js
+++ src/common/components/Form/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Form.module.css';
 
 const Form = (props) => (
-    <form className={styles.Form} data-netlify="true" name={props.name} method="POST" data-netlify-honeypot="bot-field" action={props.action}>
+    <form className={styles.Form} data-netlify="true" name={props.name} method="POST" action={props.action}>
         <input type="hidden" name="form-name" value={props.name} />
         {props.children}
     </form>
